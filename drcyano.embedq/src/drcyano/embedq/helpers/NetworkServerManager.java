9e7c98879d801a87f120de246045bc8aff80e489
DrCyano <explosivegnome@yahoo.com>
2019-12-12 15:04:30 +1100

    reduced scope, version 1.0 will not include any network communication. working on Docs for 1.0 release

--- drcyano.embedq/src/drcyano/embedq/helpers/NetworkServerManager.java
+++ drcyano.embedq/src/drcyano/embedq/helpers/NetworkServerManager.java
@@ -12,59 +12,12 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class NetworkServerManager implements Closeable {
-	private static final int networkPayloadSizeLimit = 0x10000;
+	
 	private final AtomicBoolean runControl = new AtomicBoolean(false);
-	private final Map<Socket, InputStream> tcpClientInputs = Collections.synchronizedMap(new HashMap<>());
-	private final Map<Socket, OutputStream> tcpClientOutputs = Collections.synchronizedMap(new HashMap<>());
-	private final Map<Socket, ByteBuffer> tcpClientbuffers = Collections.synchronizedMap(new HashMap<>());
 	
-	private void tcpListenLoop(){
-		while(runControl.get()){
-			for(Map.Entry<Socket, InputStream> client : tcpClientInputs.entrySet()){
-				Socket sock  = client.getKey();
-				if(sock.isClosed()){
-					dropSocket(sock);
-					continue;
-				}
-				readSocketBlock:
-				{
-					InputStream in = client.getValue();
-					ByteBuffer bb = tcpClientbuffers
-					while (in.available() > 0) {
-						if(bb.position() >= networkPayloadSizeLimit){
-							// payload too big (invalid)
-							oversizedPayloadError(sock);
-							clearBuffer(sock);
-							break readSocketBlock;
-						}
-						int bin = in.read();
-						if (bin < 0) {
-							// end of stream
-							closeClient(sock);
-							break readSocketBlock;
-						}
-						bb.put((byte) bin);
-						boolean done = Protocol.isCompletePayload(bb);
-					}
-				}
-			}
-		}
-	}
-	
-	private void clearBuffer(Socket sock) {
+	public NetworkServerManager(){
 		throw new UnsupportedOperationException("not implemented yet");
-	}
-	
-	private void oversizedPayloadError(Socket sock) {
-		throw new UnsupportedOperationException("not implemented yet");
-	}
-	
-	private void closeClient(Socket sock) {
-		throw new UnsupportedOperationException("not implemented yet");
-	}
-	
-	private void dropSocket(Socket sock) {
-		throw new UnsupportedOperationException("not implemented yet");
+		
 	}
 	
 	@Override
