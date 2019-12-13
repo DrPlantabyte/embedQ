24274ccceabb295269fbede1a8f7f788df743123
DrCyano <explosivegnome@yahoo.com>
2019-12-13 17:38:37 +1100

    license change

--- drcyano.embedq/src/drcyano/embedq/data/MessageOutputStream.java
+++ drcyano.embedq/src/drcyano/embedq/data/MessageOutputStream.java
@@ -1,7 +1,20 @@
 package drcyano.embedq.data;
+/*
+This file is part of EmbedQ.
 
-import drcyano.embedq.data.Message;
-import drcyano.embedq.data.Topic;
+EmbedQ is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+EmbedQ is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with EmbedQ.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
--- drcyano.embedq/src/drcyano/embedq/exceptions/InvalidTopicStringException.java
+++ drcyano.embedq/src/drcyano/embedq/exceptions/InvalidTopicStringException.java
@@ -1,5 +1,20 @@
 package drcyano.embedq.exceptions;
+/*
+This file is part of EmbedQ.
 
+EmbedQ is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+EmbedQ is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with EmbedQ.  If not, see <https://www.gnu.org/licenses/>.
+ */
 /**
  * This exception is thrown to indicate that a <code>Topic</code> string is not valid.
  */
